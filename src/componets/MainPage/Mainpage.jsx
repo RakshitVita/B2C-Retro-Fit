574657cf85cceb2b84b0b1b546c9db06d65969f8
Ronit Fulari <145636323+Ronitfulari31@users.noreply.github.com>
2025-05-15 11:39:37 +0530

    secondcommit

---
+++ src/componets/MainPage/Mainpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Maincontainer from './Maincontainer'
+const Mainpage = () => {
+  return (
+    <>
+  <Maincontainer>Mainpage by ronit</Maincontainer>
+        
+    
+    </>
+  )
+}
+
+export default Mainpage
