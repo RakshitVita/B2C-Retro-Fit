26416df1e01769c00cca6e130f2381770e2e4544
ganesh chate <ganeshchate2004@gmail.com>
2025-05-16 12:32:39 +0530

    final change
    final change

--- src/componets/Download.jsx
+++ src/componets/Download.jsx
@@ -1,19 +1,97 @@
+
+
+
+
 import React from 'react'
+import Maincontainer from './Maincontainer'
 
-
-
-const Download  = ({ children }) => {
+const Download = () => {
   return (
-     <div className="flex justify-center items-center min-h-[80vh] px-4">
-      <div className="bg-white rounded-xl shadow-lg p-8 w-full  max-w-5xl">
-        {children}
-       Download
-      </div>
-    </div>
+    <>
+    <Maincontainer>
+        Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+Introduction
+
+Online Clipboard is free online tool that help you to copy and paste between devices. It allows you to copy text or image.
+
+How to use:
+
+1. choose your text or image that would be like to be copy to other device by putting your text in the following text box, or just copy your text or image.
+
+2. Send your text or image to Online Clipboard by the following button.
+
+3. At the other device, retrieve your text or image from Online Clipboard by entering your code.
+    </Maincontainer>
+    </>
   )
 }
 
-export default Download 
+export default Download
 
 
 
